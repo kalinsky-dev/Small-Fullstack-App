a37447a9245ce2b139ca1e3294a2c7fef84845a5
Kalin Hristov <kalin0169@gmail.com>
2023-10-01 12:27:29 +0300

    Create an array from all  my Products in my Backend.
    Creating a mocking DB.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 
+const products = [
+  {
+    name: 'Product1',
+    price: 110,
+  },
+  {
+    name: 'Product2',
+    price: 75,
+  },
+  {
+    name: 'Product3',
+    price: 165,
+  },
+];
+
 const app = express();
 
 app.use(express.static('static'));
 
 app.get('/data', (req, res) => {
-  res.json([
-    {
-      name: 'Product1',
-      price: 110,
-    },
-    {
-      name: 'Product2',
-      price: 75,
-    },
-    {
-      name: 'Product3',
-      price: 165,
-    },
-  ]);
+  res.json(products);
 });
 
 app.listen(3000);
