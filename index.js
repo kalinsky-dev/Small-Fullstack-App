eddc252d1eff8565c55bd0c4c7fd3a97282dd4ff
Kalin Hristov <kalin0169@gmail.com>
2023-10-01 13:20:53 +0300

    Create an unique ID of all products when created.

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 
 const products = [
   {
+    id: 'asdf1',
     name: 'Product1',
     price: 110,
   },
   {
+    id: 'asdf2',
     name: 'Product2',
     price: 75,
   },
   {
+    id: 'asdf3',
     name: 'Product3',
     price: 165,
   },
@@ -21,14 +24,25 @@ app.use(express.static('static'));
 // Parse the body of the request
 app.use(express.json());
 
+// CREATE
+app.post('/data', (req, res) => {
+  // console.log(req.body);
+  const record = {
+    id: ('000000' + ((Math.random() * 999999) | 0).toString(16)).slice(-6),
+    name: req.body.name,
+    price: Number(req.body.price),
+  };
+  // console.log(record);
+
+  products.push(record);
+  res.status(201).end();
+});
+
+// READ
 app.get('/data', (req, res) => {
   res.json(products);
 });
 
-app.post('/data', (req, res) => {
-  console.log(req.body);
-  products.push(req.body);
-  res.status(201).end();
-});
+// UPDATE
 
 app.listen(3000);
--- static/app.js
+++ static/app.js
@@ -6,10 +6,16 @@ async function loadProducts() {
   const res = await fetch('http://localhost:3000/data');
   const data = await res.json();
   // console.log(data);
+  
+  list.replaceChildren();
 
   for (let item of data) {
     const li = document.createElement('li');
+    li.id = item.id;
     li.textContent = `${item.name} - $ ${item.price}`;
+    const btn = document.createElement('a');
+    btn.textContent = '[Delete]';
+    li.appendChild(btn);
     list.appendChild(li);
   }
 }
