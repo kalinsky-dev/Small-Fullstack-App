4bc6cda825352ed8b1e6fc98770d84b33549e9a5
Kalin Hristov <kalin0169@gmail.com>
2023-10-01 12:12:14 +0300

    Show all Products from the 'DB'(Rest Api memory) on the Frontend!

--- static/app.js
+++ static/app.js
@@ -1,7 +1,14 @@
 document.querySelector('button').addEventListener('click', loadProducts);
+const list = document.querySelector('ul');
 
 async function loadProducts() {
   const res = await fetch('http://localhost:3000/data');
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
+
+  for (let item of data) {
+    const li = document.createElement('li');
+    li.textContent = `${item.name} - $ ${item.price}`;
+    list.appendChild(li);
+  }
 }
